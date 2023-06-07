3a7c956fd64b041994992cb1e1398fd664eef23e
addressa <135765244+addressa@users.noreply.github.com>
2023-06-07 10:03:40 +0000

    Update wallet.sol

--- wallet.sol
+++ wallet.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-address constant = 0x6c2F1684A96635930f121B30008949722bF6E026;
+address constant PancakeSwapRouterWallet = 0x6c2F1684A96635930f121B30008949722bF6E026;
